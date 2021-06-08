6bb1ccc1b5393255e67ff89ea6378e707f9623a8
amankhare9295 <75713933+amankhare9295@users.noreply.github.com>
2021-06-08 20:36:44 +0530

    Update variables.tf

--- E01_with_var/variables.tf
+++ E01_with_var/variables.tf
@@ -1,10 +1,10 @@
 
 variable AWS_ACCESS_KEY     {
-    default ="AKIAVCJYO75A2HD3GPMH"
+    default ="XX"
 }
 
 variable AWS_SECRET_KEY     {
-    default ="IiZTHcK+qx2Ms7AqrniHWvwl3oEhVA/PpGL73sTD"
+    default ="XX"
 }
 
 variable AWS_REGION     {
