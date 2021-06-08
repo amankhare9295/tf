8efe182a0b8a9151c6afb98705271e4565731d32
Aman Khare <amankhare9295@gmail.com>
2021-06-08 11:02:37 +0530

    first commmit

---
+++ E01_with_var/variables.tf
@@ -0,0 +1,12 @@
+
+variable AWS_ACCESS_KEY     {
+    default ="AKIAVCJYO75A2HD3GPMH"
+}
+
+variable AWS_SECRET_KEY     {
+    default ="IiZTHcK+qx2Ms7AqrniHWvwl3oEhVA/PpGL73sTD"
+}
+
+variable AWS_REGION     {
+    default ="ap-south-1"
+}
